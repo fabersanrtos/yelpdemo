e670dc7d065c0d2e032ddf4b9a2227e8980a1b2b
FaberSantos <faber@dynamicfactor.com>
2016-02-15 14:18:11 +0800

    Create Review Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215042749) do
+ActiveRecord::Schema.define(version: 20160215061124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160215042749) do
     t.datetime "updated_at", null: false
     t.string   "phone"
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
